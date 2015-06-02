9f8df9153c722dda0a5b75777ff61c124c904956
Bernardo Chaves <opingow@gmail.comprecomposeunicode = true>
2015-06-01 21:34:40 -0300

    Remove failed experiments.

--- ruby/toml_parser/lib/toml_parser/grammar.rb
+++
@@ -1,4 +0,0 @@
-module TomlParser
-  class Grammar
-  end
-end
--- ruby/toml_parser/test/helper.rb
+++
@@ -1,4 +0,0 @@
-require "minitest/autorun"
-require_relative "../lib/toml_parser/tokenizer"
-require_relative "../lib/toml_parser/token"
-require_relative "../lib/toml_parser/expression"
