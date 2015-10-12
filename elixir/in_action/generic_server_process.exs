ce9f41e5bbe3d42a36f227504b723b7749009515
Bernardo Chaves <bernardo.chaves@plataformatec.com.br>
2015-10-11 21:34:30 -0300

    Implement KeyValueStore using GenServer module.

--- elixir/in_action/generic_server_process.exs
+++ elixir/in_action/generic_server_process.exs
@@ -49,10 +49,6 @@ defmodule KeyValueStore do
     ServerProcess.call(pid, {:get, key})
   end
 
-  def handle_call({:put, key, value}, state) do
-    {:ok, HashDict.put(state, key, value)}
-  end
-
   def handle_call({:get, key}, state) do
     {HashDict.get(state, key), state}
   end
