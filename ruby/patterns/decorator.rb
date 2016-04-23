85523a8b200d5886b48598b083441d7f92010f27
Bernardo Chaves <bernardo.amc@gmail.com>
2016-04-23 20:48:40 -0300

    Add decorator pattern.

---
+++ ruby/patterns/decorator.rb
@@ -0,0 +1,60 @@
+# The Decorator pattern is used to increment an object, or in other words,
+# to add layer features on to an object.
+#
+# Let's see an example:
+
+class SimpleWriter
+  def initialize(path)
+    @file = File.open(path, 'w')
+  end
+
+  def write_line(line)
+    @file.print(line)
+    @file.print("\n")
+  end
+
+  def pos
+    @file.pos
+  end
+
+  def rewind
+    @file.rewind
+  end
+
+  def close
+    @file.close
+  end
+end
+
+require 'forwardable'
+
+class WriterDecorator
+  extend Forwardable
+
+  def_delegators :@real_writer, :write_line, :rewind, :pos, :close
+
+  def initialize(real_writer)
+    @real_writer = real_writer
+  end
+end
+
+class NumberingWriter < WriterDecorator
+  def initialize(real_writer)
+    super(real_writer)
+    @line_number = 1
+  end
+
+  def write_line(line)
+    @real_writer.write_line("#{@line_number}: #{line}")
+    @line_number += 1
+  end
+end
+
+class TimeStampingWriter < WriterDecorator
+  def write_line(line)
+    @real_writer.write_line("#{Time.new}: #{line}")
+  end
+end
+
+writer = TimeStampingWriter.new(NumberingWriter.new(SimpleWriter.new('test.txt')))
+writer.write_line('This is a test')
