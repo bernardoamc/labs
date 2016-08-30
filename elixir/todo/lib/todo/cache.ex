1e29a021c601899f4117b8aac94a85856f1178ee
Bernardo Araujo <bernardo.amc@gmail.com>
2016-08-30 00:42:09 -0400

    Add initial version of todo-list project.

---
+++ elixir/todo/lib/todo/cache.ex
@@ -0,0 +1,31 @@
+defmodule Todo.Cache do
+  use GenServer
+
+  def start do
+    GenServer.start(__MODULE__, nil)
+  end
+
+  def server_process(cache_pid, todo_list_name) do
+    GenServer.call(cache_pid, {:server_process, todo_list_name})
+  end
+
+  def init(_) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:server_process, todo_list_name}, _, todo_servers) do
+    case Map.fetch(todo_servers, todo_list_name) do
+      {:ok, todo_server} ->
+        {:reply, todo_server, todo_servers}
+
+      :error ->
+        {:ok, new_server} = Todo.Server.start
+
+        {
+          :reply,
+          new_server,
+          Map.put(todo_servers, todo_list_name, new_server)
+        }
+    end
+  end
+end
