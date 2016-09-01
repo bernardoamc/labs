ed92135b7d0db01ffe2b18e8ed849da09f10bf32
Bernardo Araujo <bernardo.amc@gmail.com>
2016-08-31 22:28:10 -0400

    Implement database workers to avoid the current bottleneck on DatabaseServer.

--- elixir/todo/lib/todo/database.ex
+++ elixir/todo/lib/todo/database.ex
@@ -6,37 +6,45 @@ defmodule Todo.Database do
   end
 
   def store(key, data) do
-    GenServer.cast(:database_server, {:store, key, data})
+    key
+    |> get_worker
+    |> Todo.DatabaseWorker.store(key, data)
   end
 
   def get(key) do
-    GenServer.call(:database_server, {:get, key})
+    key
+    |> get_worker
+    |> Todo.DatabaseWorker.get(key)
+  end
+
+  def get_worker(key) do
+    GenServer.call(:database_server, {:get_worker, key})
   end
 
   def init(db_folder) do
-    File.mkdir_p(db_folder)
-    {:ok, db_folder}
+    workers = start_workers(db_folder)
+    {:ok, workers}
   end
 
-  def handle_cast({:store, key, data}, db_folder) do
-    file_name(db_folder, key)
-    |> File.write!(:erlang.term_to_binary(data))
-
-    {:noreply, db_folder}
-  end
-
-  def handle_call({:get, key}, _, db_folder) do
-    data = case File.read(file_name(db_folder, key)) do
-      {:ok, contents} -> :erlang.binary_to_term(contents)
-      _ -> nil
+  defp start_workers(db_folder) do
+    for index <- 1..3, into: %{} do
+      {:ok, pid} = Todo.DatabaseWorker.start(db_folder)
+      {index - 1, pid}
     end
+  end
 
-    {:reply, data, db_folder}
+  def handle_call({:get_worker, key}, _, workers) do
+    worker_index = :erlang.phash2(key, 3)
+    {:reply, Map.get(workers, worker_index), workers}
   end
 
   # Needed for testing purposes
-  def handle_info(:stop, state), do: {:stop, :normal, state}
-  def handle_info(_, state), do: {:noreply, state}
+  def handle_info(:stop, workers) do
+    workers
+    |> Map.values
+    |> Enum.each(&send(&1, :stop))
 
-  defp file_name(db_folder, key), do: "#{db_folder}/#{key}"
+    {:stop, :normal, %{}}
+  end
+  def handle_info(_, state), do: {:noreply, state}
 end
