ee088900b0b8181bd8594d1d19ca5dda0c8f0177
Bernardo Araujo <bernardo.amc@gmail.com>
2018-08-06 21:25:18 -0400

    Ruby sign url

---
+++ ruby/sign_url.rb
@@ -0,0 +1,14 @@
+require 'openssl'
+require 'base64'
+require 'rack/utils'
+
+HMAC_SECRET = "super_secret".freeze
+
+def sign_message(message)
+  digest = OpenSSL::Digest.new('sha256')
+  OpenSSL::HMAC.hexdigest(digest, HMAC_SECRET, message)
+end
+
+host =  'shop1.myshopify.io'
+puts "Valid host: #{Base64.urlsafe_encode64(host).sub(/=+\z/, '')}"
+puts "Valid hmac: #{sign_message(host)}"
